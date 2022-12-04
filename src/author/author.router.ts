7ca88da236bedf9175515d4a104977ddc59c7da5
Juyotal <cesarkamga3@gmail.com>
2022-12-04 09:04:26 +0200

    initial commit

---
+++ src/author/author.router.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import type { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+import * as AuthorService from "./author.service"
+import { request } from "http";
+
+export const authorRouter = express.Router();
+
+
+authorRouter.get("/", async (request: Request, response: Response) => {
+    try {
+        const authors = await AuthorService.listAuthors();
+        return response.status(200).json(authors);
+    } catch (error: any) {
+        return response.status(500).json(error.message);
+    }
+});
+
+authorRouter.get("/:id", async (request:Request, response:Response) => {
+    const id: number = parseInt(request.params.id, 10);
+
+    try {
+        const author = await AuthorService.getAuthor(id)
+        if (author) {
+            return response.status(200).json(author)
+        }
+
+        return response.status(404).json("Author could not be found")
+    } catch (error: any){
+        return response.status(500).json(error.message);
+    }
+})
